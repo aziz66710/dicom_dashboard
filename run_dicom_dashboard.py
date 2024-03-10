bc19d7ee10d11696ab6aa5c6bad18c56a58b4a7e
Aziz Uddin <azizuddin@vitaamedical.com>
2024-03-10 10:25:55 -0400

    fixed radio buttons and added button for original image and cleaned code

--- run_dicom_dashboard.py
+++ run_dicom_dashboard.py
@@ -8,23 +8,25 @@ import functools
 from dataclasses import dataclass
 import altair as alt 
 import matplotlib
+import cv2
 
-c1 = st.empty()
-c2 = st.empty()
-col1, col2 = st.columns(2)
-col3, col4 = st.columns(2)
+container_1 = st.empty()
+container_2 = st.empty()
+column_1, column_2 = st.columns(2)
+column_3, column_4 = st.columns(2)
 
 def run_workflow() -> None:
     welcome_message()
-    uploaded_file = c2.file_uploader('UPLOAD DICOM')
+    uploaded_file = container_2.file_uploader('UPLOAD DICOM')
     if uploaded_file is not None:
-        ds, ds_array = display_dicom_image(uploaded_file)
-        df = display_dicom_metadata(ds)
-        side_by_side(ds_array, df)
+        ds, ds_array = load_dicom_image(uploaded_file)
+        ds_array = image_processing_techniques(ds_array)
+        side_by_side(ds_array)
         display_image_histogram(ds_array)
+        df = display_dicom_metadata(ds)
 
 def welcome_message():
-    c1.write("""
+    container_1.write("""
 # DICOM Dashboard
 
 Upload your DICOM data file using the Upload button below and visualize your image!
@@ -34,12 +36,48 @@ def upload_button():
     uploaded_file = st.file_uploader('UPLOAD DICOM')
     return uploaded_file
     
-def display_dicom_image(uploaded_file) -> None:
+def load_dicom_image(uploaded_file) -> None:
     ds = pydicom.dcmread(uploaded_file)
     ds_array = ds.pixel_array
-    xmax = float(np.max(ds_array))
-    ymax = float(np.size(ds_array))
     return ds, ds_array
+
+def image_processing_techniques(ds_array):
+    processing_techniques = ["Original",
+                            "Mean Filter", 
+                            "Median Filter", 
+                            "Gaussian Smoothing"
+                            ]
+    column_2.header("Image Filtering")
+    filter_tech = column_2.radio("Select an Image Filter",
+                processing_techniques,
+                captions = ["Original unfiltered image",
+                            "Noise Reduction using mean of neighborhood",
+                            "Noise Reduction using median of neighborhood", 
+                            "Noise Reduction using convolution with a Gaussian smoothing kernel"])
+    if filter_tech == 'Original':
+        return ds_array
+    elif filter_tech == 'Mean Filter':
+        window=(5, 5)
+        return cv2.blur(ds_array, window)
+    elif filter_tech == 'Median Filter':
+        window=3
+        return cv2.medianBlur(ds_array, window)
+    elif filter_tech == 'Gaussian Smoothing':
+        window=(5, 5)
+        return cv2.GaussianBlur(ds_array, window, 0)
+    else:
+        return None
+
+def side_by_side(ds_array) -> None:
+    column_1.header("Image")
+    column_1.image(ds_array, caption="DICOM Images", use_column_width=True)
+    
+def display_image_histogram(ds_array) -> None:
+    fig = plot_fig(ds_array, column_4)
+    column_3.header("Intensity Histogram")
+    column_3.pyplot(fig)
+
+    return fig
 
 def display_dicom_metadata(ds) -> None:
     common_metadata: dict = {
@@ -60,19 +98,10 @@ def display_dicom_metadata(ds) -> None:
     }
     df = pd.DataFrame(common_metadata)
     df = df.replace(r'^\s*$', np.nan, regex=True)
+    st.header("DICOM Metadata")
+    st.table(df)
+
     return df
-
-def side_by_side(ds_array, df) -> None:
-    col1.header("Image")
-    col1.image(ds_array, caption="DICOM Images", use_column_width=True)
-    col2.header("DICOM Tags")
-    col2.table(df)
-
-def display_image_histogram(ds_array) -> None:
-    fig = plot_fig(ds_array, col4)
-    col3.pyplot(fig)
-
-    return fig
 
 def plot_fig(array,col):
     fig, ax = plt.subplots()
@@ -80,19 +109,19 @@ def plot_fig(array,col):
     xlim1, xlim2, ylim1, ylim2 = slider_values(col,counts,bins)
     plt.xlim([xlim1,xlim2])
     plt.ylim([ylim1,ylim2])
-    plt.title('Image Intesity Histogram')
     plt.xlabel('Intensities')
     plt.ylabel('Count')
     return fig
 
-def slider_values(col, counts, bins):
-    xlim1, xlim2 = col.slider(
+def slider_values(column, counts, bins):
+    column.header("Bin and Count Range")
+    xlim1, xlim2 = column.slider(
         'Select a x range of values',
         0.0, np.max(bins), (0.0,np.max(bins)))
-    ylim1, ylim2 = col.slider(
+    ylim1, ylim2 = column.slider(
         'Select a y range of values',
         0.0, np.max(counts), (0.0,np.max(counts)))
-    return xlim1, xlim2, ylim1, ylim2
+    return xlim1, xlim2, ylim1, ylim2    
 
 if __name__ == "__main__":
     run_workflow()
